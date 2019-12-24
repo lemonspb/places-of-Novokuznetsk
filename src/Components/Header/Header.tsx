896c8cdba837997391631451d62d6ae13d8c0dd8
Misha Skopenko <misha.skopenko@yandex.ru>
2019-12-25 01:12:05 +0300

    +

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -9,17 +9,14 @@ import firebases from '../../services/base'
   const { currentUser } = useContext(AuthContext);
   const menu = (
     <Menu className='header__menu'>
-        <Menu.Item key="1">
+      
+      <Menu.Item key="1">
      
-{currentUser?currentUser.displayName:''}                  
-         </Menu.Item>
-      <Menu.Item key="2">
-     
-  {currentUser?<Link to='/edit-account' >  <Icon type="edit" /> Редактировать</Link>:<Link to='/signup'><Icon type="user-add" />Зарегистрироваться</Link>  }
+  {currentUser?<Link to='/edit-account' >Редактировать</Link>:<Link to='/signup'>Зарегистрироваться</Link>  }
        
       </Menu.Item>
-      <Menu.Item key="3">
-      {currentUser?<Link to='/' onClick={()=>{firebases.auth().signOut()}}> <Icon type="close" /> выйти</Link>:<Link to='/login' > <Icon type="check" /> Войти</Link>  }
+      <Menu.Item key="2">
+      {currentUser?<Link to='/' onClick={()=>{firebases.auth().signOut()}}> выйти</Link>:<Link to='/login' >  Войти</Link>  }
       </Menu.Item>
     </Menu>
   );
@@ -29,11 +26,13 @@ import firebases from '../../services/base'
       <div className='header'>
           <Avatar size="large"  icon='info' className='header__info'  />
           <Dropdown overlay={menu} className='header__dropdown'>
-          {currentUser && currentUser.photoURL?<Avatar size="large"  src={currentUser.photoURL} />:<Avatar size="large" icon='user' />} 
-
-         
+            <div className='header__avatar-wrap'>
+            {currentUser && currentUser.photoURL?<Avatar size="large"  src={currentUser.photoURL} />:<Avatar size="large" icon='user' />} 
+            <div className='header__diplayname'>{currentUser?currentUser.displayName:''}</div>   
+            </div>
           
     </Dropdown>
+    
       </div>    
     );
   }
